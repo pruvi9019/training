595d0626afca3f157f46737c69f9511483706a9c
amichael03 <adamdmichael@gmail.com>
2020-08-05 18:02:54 -0500

    Making sure all is setup to help remotely

--- src/com/kotapatti/aws/practice/Practice.java
+++ src/com/kotapatti/aws/practice/Practice.java
@@ -2,6 +2,6 @@ package com.kotapatti.aws.practice;
 
 public class Practice {
     public static void main(String[] args) {
-        System.out.println("Practice Test!");
+        System.out.println("Practice Test, Hello From Tutor!");
     }
 }
